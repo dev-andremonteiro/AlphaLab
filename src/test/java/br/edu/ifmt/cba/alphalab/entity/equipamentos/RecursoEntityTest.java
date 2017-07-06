2aa4b67c9f7c1c4e15d7e0cee3ae72ac2c1ab8f4
aluno <aluno@DAI-316504.labs.dai.ifmt.edu.br>
2017-07-06 14:08:23 -0400

    Adicionada a classe exemplo de teste.

---
+++ src/test/java/br/edu/ifmt/cba/alphalab/entity/equipamentos/RecursoEntityTest.java
@@ -0,0 +1,36 @@
+package br.edu.ifmt.cba.alphalab.entity.equipamentos;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RecursoEntityTest {
+    
+    public RecursoEntityTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
