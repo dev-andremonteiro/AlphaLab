346028f39e0f1f0da87d51fa9a4b805ec6db6274
Thales Alexsander Closs <thales.a.closs@gmail.com>
2017-07-14 16:50:04 -0400

    Solicitação de instalação de software

---
+++ src/main/java/br/edu/ifmt/cba/alphalab/dao/ISolicitacaoSoftware.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifmt.cba.alphalab.dao;
+
+import br.edu.ifmt.cba.alphalab.entity.software.SolicitacaoSoftwareEntity;
+
+/**
+ *
+ * @author tcloss
+ */
+public interface ISolicitacaoSoftware extends IDAO<SolicitacaoSoftwareEntity>{
+    
+    
+    
+}
---
+++ src/main/java/br/edu/ifmt/cba/alphalab/entity/software/situacaoInstalacaoEnum.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifmt.cba.alphalab.entity.software;
+
+/**
+ *
+ * @author tcloss
+ */
+public enum situacaoInstalacaoEnum {
+    EM_ANDAMENTO("Em andamento");
+    
+
+    private String descricao;
+    private situacaoInstalacaoEnum(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+    
+    
+    
+    
+    
+}
