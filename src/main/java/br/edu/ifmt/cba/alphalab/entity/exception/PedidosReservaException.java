12f33c6b840c906c4c9239435f24ea9f472b3b5e
stevillis <stevillis@hotmail.com>
2017-07-13 21:11:33 -0400

    DAO Pedidos de Reserva

---
+++ src/main/java/br/edu/ifmt/cba/alphalab/entity/exception/PedidosReservaException.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.cba.alphalab.entity.exception;
+
+/**
+ * 
+ * @author Stévillis Sousa
+ *
+ */
+
+public class PedidosReservaException extends Exception{
+	public PedidosReservaException(String msg){
+		super(msg);		
+	}
+}
---
+++ src/main/java/br/edu/ifmt/cba/alphalab/entity/laboratorio/EnumTipoReserva.java
@@ -0,0 +1,11 @@
+package br.edu.ifmt.cba.alphalab.entity.laboratorio;
+
+/**
+ * 
+ * @author Stévillis Sousa
+ *
+ */
+public enum EnumTipoReserva {
+	DIARIA, SEMANAL, QUINZENAL, MENSAL, SEMESTRAL
+
+}
