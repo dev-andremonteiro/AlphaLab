84cbe74dfe6c194c96087be8854f6165be659b26
stevillis <stevillis@hotmail.com>
2017-07-21 09:08:11 -0400

    validar() de ReservaEntity.

--- src/main/java/br/edu/ifmt/cba/alphalab/entity/laboratorio/ReservaEntity.java
+++ src/main/java/br/edu/ifmt/cba/alphalab/entity/laboratorio/ReservaEntity.java
@@ -1,177 +1,212 @@
 package br.edu.ifmt.cba.alphalab.entity.laboratorio;
 
-import br.edu.ifmt.cba.alphalab.entity.IEntity;
-import br.edu.ifmt.cba.alphalab.entity.exception.ReservaException;
-import br.edu.ifmt.cba.alphalab.entity.pessoa.ServidorEntity;
 import java.util.ArrayList;
 import java.util.Date;
 
+import br.edu.ifmt.cba.alphalab.entity.IEntity;
+import br.edu.ifmt.cba.alphalab.entity.exception.ReservaException;
+import br.edu.ifmt.cba.alphalab.entity.pessoa.ServidorEntity;
+
 public class ReservaEntity implements IEntity<ReservaException> {
 
-    private Long id;
+	private Long id;
 
-    private EnumReserva status;
+	private EnumReserva status;
 
-    private Date dataSolicitacao;
+	private Date dataSolicitacao;
 
-    private String disciplina;
+	private String disciplina;
 
-    private String turma;
+	private String turma;
 
-    private String observacao;
+	private String observacao;
 
-    private boolean fixo;
+	private boolean fixo;
 
-    private Date dataInicio;
+	private Date dataInicio;
 
-    private Date dataFim;
+	private Date dataFim;
 
-    private Date dataAprovacaoRecusa;
+	private Date dataAprovacaoRecusa;
 
-    private String justificativa;
+	private String justificativa;
 
-    private LaboratorioEntity laboratorio;
+	private LaboratorioEntity laboratorio;
 
-    private DepartamentoEntity departamentoAula;
+	private DepartamentoEntity departamentoAula;
 
-    private ServidorEntity solicitante;
+	private ServidorEntity solicitante;
 
-    private ServidorEntity aprovador;
+	private ServidorEntity aprovador;
 
-    private ArrayList<RequisitoEntity> requisitos;
+	private ArrayList<RequisitoEntity> requisitos;
 
-    @Override
-    public Long getId() {
-        return id;
-    }
+	@Override
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Enum getStatus() {
-        return status;
-    }
+	public Enum getStatus() {
+		return status;
+	}
 
-    public void setStatus(EnumReserva status) {
-        this.status = status;
-    }
+	public void setStatus(EnumReserva status) {
+		this.status = status;
+	}
 
-    public Date getDataSolicitacao() {
-        return dataSolicitacao;
-    }
+	public Date getDataSolicitacao() {
+		return dataSolicitacao;
+	}
 
-    public void setDataSolicitacao(Date dataSolicitacao) {
-        this.dataSolicitacao = dataSolicitacao;
-    }
+	public void setDataSolicitacao(Date dataSolicitacao) {
+		this.dataSolicitacao = dataSolicitacao;
+	}
 
-    public String getDisciplina() {
-        return disciplina;
-    }
+	public String getDisciplina() {
+		return disciplina;
+	}
 
-    public void setDisciplina(String disciplina) {
-        this.disciplina = disciplina;
-    }
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
 
-    public String getTurma() {
-        return turma;
-    }
+	public String getTurma() {
+		return turma;
+	}
 
-    public void setTurma(String turma) {
-        this.turma = turma;
-    }
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
 
-    public String getObservacao() {
-        return observacao;
-    }
+	public String getObservacao() {
+		return observacao;
+	}
 
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
 
-    public boolean isFixo() {
-        return fixo;
-    }
+	public boolean isFixo() {
+		return fixo;
+	}
 
-    public void setFixo(boolean fixo) {
-        this.fixo = fixo;
-    }
+	public void setFixo(boolean fixo) {
+		this.fixo = fixo;
+	}
 
-    public Date getDataInicio() {
-        return dataInicio;
-    }
+	public Date getDataInicio() {
+		return dataInicio;
+	}
 
-    public void setDataInicio(Date dataInicio) {
-        this.dataInicio = dataInicio;
-    }
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
 
-    public Date getDataFim() {
-        return dataFim;
-    }
+	public Date getDataFim() {
+		return dataFim;
+	}
 
-    public void setDataFim(Date dataFim) {
-        this.dataFim = dataFim;
-    }
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
 
-    public Date getDataAprovacaoRecusa() {
-        return dataAprovacaoRecusa;
-    }
+	public Date getDataAprovacaoRecusa() {
+		return dataAprovacaoRecusa;
+	}
 
-    public void setDataAprovacaoRecusa(Date dataAprovacaoRecusa) {
-        this.dataAprovacaoRecusa = dataAprovacaoRecusa;
-    }
+	public void setDataAprovacaoRecusa(Date dataAprovacaoRecusa) {
+		this.dataAprovacaoRecusa = dataAprovacaoRecusa;
+	}
 
-    public String getJustificativa() {
-        return justificativa;
-    }
+	public String getJustificativa() {
+		return justificativa;
+	}
 
-    public void setJustificativa(String justificativa) {
-        this.justificativa = justificativa;
-    }
+	public void setJustificativa(String justificativa) {
+		this.justificativa = justificativa;
+	}
 
-    public LaboratorioEntity getLaboratorio() {
-        return laboratorio;
-    }
+	public LaboratorioEntity getLaboratorio() {
+		return laboratorio;
+	}
 
-    public void setLaboratorio(LaboratorioEntity laboratorio) {
-        this.laboratorio = laboratorio;
-    }
+	public void setLaboratorio(LaboratorioEntity laboratorio) {
+		this.laboratorio = laboratorio;
+	}
 
-    public DepartamentoEntity getDepartamentoAula() {
-        return departamentoAula;
-    }
+	public DepartamentoEntity getDepartamentoAula() {
+		return departamentoAula;
+	}
 
-    public void setDepartamentoAula(DepartamentoEntity departamentoAula) {
-        this.departamentoAula = departamentoAula;
-    }
+	public void setDepartamentoAula(DepartamentoEntity departamentoAula) {
+		this.departamentoAula = departamentoAula;
+	}
 
-    public ServidorEntity getSolicitante() {
-        return solicitante;
-    }
+	public ServidorEntity getSolicitante() {
+		return solicitante;
+	}
 
-    public void setSolicitante(ServidorEntity solicitante) {
-        this.solicitante = solicitante;
-    }
+	public void setSolicitante(ServidorEntity solicitante) {
+		this.solicitante = solicitante;
+	}
 
-    public ServidorEntity getAprovador() {
-        return aprovador;
-    }
+	public ServidorEntity getAprovador() {
+		return aprovador;
+	}
 
-    public void setAprovador(ServidorEntity aprovador) {
-        this.aprovador = aprovador;
-    }
+	public void setAprovador(ServidorEntity aprovador) {
+		this.aprovador = aprovador;
+	}
 
-    public ArrayList<RequisitoEntity> getRequisitos() {
-        return requisitos;
-    }
+	public ArrayList<RequisitoEntity> getRequisitos() {
+		return requisitos;
+	}
 
-    public void setRequisitos(ArrayList<RequisitoEntity> requisitos) {
-        this.requisitos = requisitos;
-    }
-    
-    
-    @Override
-    public ReservaException validar() {
-        return null;
-    }   
+	public void setRequisitos(ArrayList<RequisitoEntity> requisitos) {
+		this.requisitos = requisitos;
+	}
+
+	@Override
+	public ReservaException validar() {
+		StringBuilder msg = new StringBuilder();
+		if (status == null)
+			msg.append("Status do laboratório deve ser informado!\n");
+
+		if (dataSolicitacao == null)
+			msg.append("Data de solicitação da reserva deve ser informada!\n");
+
+		if (disciplina == null || disciplina.trim().length() == 0)
+			msg.append("Nome da disciplina é obrigatório!\n");
+		if (disciplina != null || disciplina.trim().length() < 5)
+			msg.append("Nome da disciplina deve possuir pelo menos 5 caracteres!\n");
+
+		if (turma == null || turma.trim().length() == 0)
+			msg.append("Nome da turma é obrigatório!\n");
+		if (turma != null || turma.trim().length() < 6)
+			msg.append("Nome da turma deve possuir pelo menos 6 caracteres!\n");
+
+		if (observacao != null || observacao.trim().length() < 10)
+			msg.append("Observação deve possuir pelo menos 10 caracteres!\n");
+
+		if (dataInicio == null)
+			msg.append("Data de início da reserva deve ser informada!\n");
+
+		if (dataFim == null)
+			msg.append("Data final da reserva deve ser informada!\n");
+
+		if (dataAprovacaoRecusa == null)
+			msg.append("Data de Aprovação ou Recusa deve ser informada!\n");
+
+		if (justificativa != null || justificativa.trim().length() < 10)
+			msg.append("Justificativa deve possuir pelo menos 10 caracteres!\n");
+
+		if (msg.length() > 0)
+			return new ReservaException(msg.toString());
+		else
+			return null;
+	}
 }
