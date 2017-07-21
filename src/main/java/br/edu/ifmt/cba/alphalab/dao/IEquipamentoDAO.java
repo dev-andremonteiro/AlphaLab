9dd5e6f25ff57fba94607b4626b746633090d860
stevillis <stevillis@hotmail.com>
2017-07-21 16:43:59 -0400

    MockEquipamentoDAO

---
+++ src/main/java/br/edu/ifmt/cba/alphalab/dao/IEquipamentoDAO.java
@@ -0,0 +1,13 @@
+package br.edu.ifmt.cba.alphalab.dao;
+
+import br.edu.ifmt.cba.alphalab.entity.equipamentos.EquipamentoEntity;
+
+/**
+ * 
+ * @author Stévillis Sousa
+ *
+ */
+
+public interface IEquipamentoDAO extends IDAO<EquipamentoEntity> {
+	EquipamentoEntity getByNome(String nome);
+}
--- src/main/java/br/edu/ifmt/cba/alphalab/dao/mock/laboratorio/MockLaboratorioDAO.java
+++ src/main/java/br/edu/ifmt/cba/alphalab/dao/mock/laboratorio/MockLaboratorioDAO.java
@@ -28,7 +28,6 @@ public class MockLaboratorioDAO implements ILaboratorioDAO {
 		/*
 		 * Esperando criação MockEquipamento...
 		 */
-
 	}
 
 	public MockLaboratorioDAO() {
