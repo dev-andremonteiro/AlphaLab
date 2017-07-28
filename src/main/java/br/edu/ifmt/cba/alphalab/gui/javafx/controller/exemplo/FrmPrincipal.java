1edde698ea90cd83aa993ba3ec72146b77ebfe05
stevillis <stevillis@hotmail.com>
2017-07-28 16:55:53 -0400

    Carregar tela FrmSolicitarReservaHorarioPorRequisito no Menu Principal.

--- src/main/java/br/edu/ifmt/cba/alphalab/gui/javafx/controller/exemplo/FrmPrincipal.java
+++ src/main/java/br/edu/ifmt/cba/alphalab/gui/javafx/controller/exemplo/FrmPrincipal.java
@@ -1,74 +1,106 @@
 package br.edu.ifmt.cba.alphalab.gui.javafx.controller.exemplo;
 
-
 import java.io.IOException;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Menu;
+import javafx.scene.Node;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 
 public class FrmPrincipal {
 
-    @FXML
-    private MenuItem mniVenda;
+	public static final String LINGUA_INGLES = "gui/i18N";
+	public static final String LINGUA_PORTUGUES = "gui/i18N_pt_BR";
 
-    @FXML
-    private MenuItem mniSair;
+	@FXML
+	private BorderPane bdpPrincipal;
 
-    @FXML
-    private Menu mnuOperacoes;
+	@FXML
+	private MenuBar mnbPrincipal;
 
-    @FXML
-    private BorderPane bdpPrincipal;
+	@FXML
+	private MenuItem mniLogin;
 
-    @FXML
-    private MenuBar mnbPrincipal;
+	@FXML
+	private MenuItem mniIngles;
 
-    @FXML
-    private MenuItem mniCliente;
+	@FXML
+	private MenuItem mniPortugues;
 
-    @FXML
-    private MenuItem mniSobre;
+	@FXML
+	private MenuItem mniSair;
 
-    @FXML
-    private Menu mnuCadastro;
+	@FXML
+	private MenuItem mniReservaPorHorario;
 
-    @FXML
-    private Menu mnuAjuda;
+	@FXML
+	private MenuItem mniReservaPorRequisito;
 
-    @FXML
-    void mniCliente_onAction(ActionEvent event) {
-        BorderPane frmCliente = null;
-        
-        try{
-            frmCliente = FXMLLoader.load(FrmCliente.class.getClassLoader().getResource("gui//FrmCliente.fxml"),ResourceBundle.getBundle("gui/i18N"));
-            bdpPrincipal.setCenter(frmCliente);
-        }catch (IOException ioe){
-            ioe.printStackTrace();
-        }    	
-    }
-   
-    
-    @FXML
-    void mniSobre_onAction(ActionEvent event) {
-        VBox frmSobre = null;
-        try{
-            frmSobre = FXMLLoader.load(FrmPrincipal.class.getClassLoader().getResource("gui//FrmSobre.fxml"),ResourceBundle.getBundle("gui/i18N"));
-            bdpPrincipal.setCenter(frmSobre);
-        }catch (IOException ioe){
-            ioe.printStackTrace();
-        }    	
-    }    
+	@FXML
+	private MenuItem mniAjuda;
 
+	@FXML
+	private MenuItem mniSobre;
 
-    @FXML
-    void mniSair_onAction(ActionEvent event) {
-    	System.exit(0);
-    }
+	@FXML
+	void mniIngles_onAction(ActionEvent event) {
 
+	}
+
+	@FXML
+	void mniLogin_onAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void mniPortugues_onAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void mniReservaPorHorario_onAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void mniReservaPorRequisito_onAction(ActionEvent event) {
+		loadPane("FrmSolicitarReservaHorarioPorRequisito");
+
+	}
+
+	@FXML
+	void mniAjuda_onAction(ActionEvent event) {
+		loadPane("FrmCliente");
+	}
+
+	@FXML
+	void mniSobre_onAction(ActionEvent event) {
+		loadPane("FrmSobre");
+	}
+
+	@FXML
+	void mniSair_onAction(ActionEvent event) {
+		System.exit(0);
+	}
+
+	/**
+	 * 
+	 * @param string
+	 *            é o caminho da tela a ser carregada. <b>Exemplo - carregar
+	 *            tela FrmCliente.fxml:</b> loadPane(FrmCliente)
+	 */
+	private void loadPane(String string) {
+		Node node = null;
+		try {
+			node = FXMLLoader.load(FrmPrincipal.class.getClassLoader().getResource("gui//" + string + ".fxml"),
+            		ResourceBundle.getBundle(FrmPrincipal.LINGUA_PORTUGUES));
+			bdpPrincipal.setCenter(node);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
 }
