63b804e809109db5272109d2f50a747fd160e176
Thales Alexsander Closs <thales.a.closs@gmail.com>
2017-08-11 20:25:19 -0400

    Implementação de buscas específicas e busca geral

--- src/main/java/br/edu/ifmt/cba/alphalab/business/SolicitacaoSoftware.java
+++ src/main/java/br/edu/ifmt/cba/alphalab/business/SolicitacaoSoftware.java
@@ -8,8 +8,10 @@ package br.edu.ifmt.cba.alphalab.business;
 import br.edu.ifmt.cba.alphalab.dao.IDAO;
 import br.edu.ifmt.cba.alphalab.dao.ISolicitacaoSoftware;
 import br.edu.ifmt.cba.alphalab.entity.exception.SolicitacaoSoftwareException;
+import br.edu.ifmt.cba.alphalab.entity.software.SituacaoSolicitacaoEnum;
 import br.edu.ifmt.cba.alphalab.entity.software.SolicitacaoSoftwareEntity;
 import br.edu.ifmt.cba.alphalab.gui.javafx.controller.software.SolicitacaoSoftwareView;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -32,5 +34,40 @@ public class SolicitacaoSoftware extends ABusiness<SolicitacaoSoftwareEntity, So
     public List<SolicitacaoSoftwareEntity> buscarPorSolicitante(String solicitante){
         return ((ISolicitacaoSoftware) dao).buscaPorServidor(solicitante);
     }
+    public List<SolicitacaoSoftwareEntity> buscarPorLaboratorio(String laboratorio){
+        return ((ISolicitacaoSoftware) dao).buscaPorLaboratorio(laboratorio);
+    }
+    public List<SolicitacaoSoftwareEntity> buscarPorSituacao(SituacaoSolicitacaoEnum situacao){
+        return ((ISolicitacaoSoftware)dao).buscaPorSituacao(situacao);
+    }
+    public List<SolicitacaoSoftwareEntity> buscaGeral(Calendar data,String solicitante,String laboratorio, SituacaoSolicitacaoEnum situacao){
+        List<SolicitacaoSoftwareEntity> retorno = new ArrayList<>();
+        if(data!=null){
+            if(retorno.isEmpty())
+                retorno=buscarPorData(data);
+        }
+        if(solicitante.trim().equals("")){
+            if(retorno.isEmpty())
+                retorno=buscarPorSolicitante(solicitante);
+            else
+                retorno.retainAll(buscarPorSolicitante(solicitante));
+        }
+        if(laboratorio.trim().equals("")){
+            if(retorno.isEmpty())
+                retorno=buscarPorLaboratorio(laboratorio);
+            else
+                retorno.retainAll(buscarPorLaboratorio(laboratorio));
+        }
+        if(situacao!=null){
+            if(retorno.isEmpty())
+                retorno=buscarPorSituacao(situacao);
+            else
+                retorno.retainAll(buscarPorSituacao(situacao));
+        }
+        return retorno;
+    }
+    
+    
+    
     
 }
