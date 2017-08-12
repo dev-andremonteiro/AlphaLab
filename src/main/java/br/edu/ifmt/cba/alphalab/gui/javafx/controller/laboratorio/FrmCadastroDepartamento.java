86b930bbe4de175c808a9a4cc5227cf06c1fa73b
Harriman Faria <harri.faria@gmail.com>
2017-08-11 23:59:25 -0400

    CRYING

--- src/main/java/br/edu/ifmt/cba/alphalab/gui/javafx/controller/laboratorio/FrmCadastroDepartamento.java
+++ src/main/java/br/edu/ifmt/cba/alphalab/gui/javafx/controller/laboratorio/FrmCadastroDepartamento.java
@@ -15,14 +15,18 @@ import br.edu.ifmt.cba.alphalab.dao.mock.servidor.MockServidorDAO;
 import br.edu.ifmt.cba.alphalab.entity.exception.DepartamentoException;
 import br.edu.ifmt.cba.alphalab.entity.laboratorio.DepartamentoEntity;
 import br.edu.ifmt.cba.alphalab.entity.pessoa.ServidorEntity;
+import br.edu.ifmt.cba.alphalab.gui.javafx.controller.exemplo.FrmPrincipal;
 import br.edu.ifmt.cba.alphalab.gui.javafx.util.Alertas;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
@@ -42,102 +46,100 @@ import javafx.scene.input.MouseEvent;
 import javax.print.DocFlavor.URL;
 
 public class FrmCadastroDepartamento implements Initializable {
-    
+
     @FXML
     private TabPane tbpDepartamento;
-    
+
     @FXML
     private Tab tabConsultarDepartamento;
-    
+
     @FXML
     private TextField txtPesqSiglaDep;
-    
+
     @FXML
     private TextField txtPesqNomeDep;
-    
+
     @FXML
     private Button btnProcurar;
-    
+
     @FXML
     private TableView<DepartamentoEntity> tbvDepartamentos;
-    
+
     @FXML
     private TableColumn<DepartamentoEntity, String> tbcSigla;
-    
+
     @FXML
     private TableColumn<DepartamentoEntity, String> tbcNome;
-    
+
     @FXML
     private TableColumn<DepartamentoEntity, String> tbcChefe;
-    
+
     @FXML
     private Button btnNovoDep;
-    
+
     @FXML
     private Button btnAlterar;
-    
+
     @FXML
     private Button btnExcluir;
-    
+
     @FXML
     private Button btnSair;
-    
+
     @FXML
     private Tab tabGerenciarDepartamento;
-    
+
     @FXML
     private TextField txtNovaSiglaDep;
-    
+
     @FXML
     private TextField txtNovoNomeDep;
-    
+
     @FXML
     private ChoiceBox<ServidorEntity> chbChefeDep;
-    
+
     @FXML
     private Button btnNovoChefeDep;
-    
+
     @FXML
     private TextArea txtObs;
-    
+
     @FXML
     private Button btnConfirmar;
-    
+
     @FXML
     private Button btnSair2;
-    
+
     private Dialog<String> dlgMensagem = new Dialog<>();
     private ButtonType bttOk = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
-    
+
     private ButtonType bttSim = new ButtonType("Sim", ButtonBar.ButtonData.OK_DONE);
     private ButtonType bttNao = new ButtonType("NÃ£o", ButtonBar.ButtonData.CANCEL_CLOSE);
     private Dialog<ButtonType> dlgConfirmacao = new Dialog<>();
-    
+
     Departamento departamento = new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO());
     private boolean novo = true;
-    
+
     @FXML
     void btnAlterar_onAction(ActionEvent event) {
         if (tbvDepartamentos.getSelectionModel().getSelectedIndex() >= 0) {
             txtObs.setDisable(false);
             habilitarEdicao(true);
-            
+
             tbpDepartamento.getSelectionModel().select(tabGerenciarDepartamento);
             novo = false;
         } else {
-            dlgMensagem.setTitle("ERRO DE SELEÃ‡ÃƒO");
-            dlgMensagem.setContentText("Ã‰ preciso selecionar um cliente da tabela para poder realizar alteraÃ§Ã£o.");
-            dlgMensagem.showAndWait();
+            Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Erro", "Erro de Seleção", "Selecione um Departamento para Alterar");
         }
     }
-    
+
     @FXML
     void btnAlterar_onKeyPressed(KeyEvent event) {
         if (event.getCode() == KeyCode.ENTER) {
             btnAlterar.fire();
         }
     }
-    
+
     @FXML
     void btnConfirmar_onAction(ActionEvent event) {
         DepartamentoEntity ent;
@@ -151,58 +153,55 @@ public class FrmCadastroDepartamento implements Initializable {
         ent.setChefe((ServidorEntity) chbChefeDep.getSelectionModel().getSelectedItem());
         DepartamentoException exc = (DepartamentoException) departamento.save(ent);
         if (exc == null) {
-            dlgMensagem.setTitle("Departamento Salvo com Sucesso");
-            dlgMensagem.setContentText("Departamento Salvo com Sucesso");
-            dlgMensagem.showAndWait();
-            
+            Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Sucesso", "Departamento Salvo", "Departamento Salvo com Sucesso");
+
             habilitarEdicao(false);
+            inicializarForm();
+            tbpDepartamento.getSelectionModel().select(tabConsultarDepartamento);
+
         } else {
-            dlgMensagem.setTitle("ERRO AO SALVAR DEPARTAMENTO");
-            dlgMensagem.setContentText("Dados Inconsistentes, favor revisar");
-            dlgMensagem.showAndWait();
+            Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Erro", "Erro ao Salvar Departamento", "Dados Inconsistentes, favor revisar");
         }
     }
-    
+
     @FXML
     void btnConfirmar_onKeyPressed(KeyEvent event) {
-        
+        if (event.getCode() == KeyCode.ENTER) {
+            btnConfirmar.fire();
+        }
     }
-    
+
     @FXML
     void btnExcluir_onAction(ActionEvent event) {
         if (tbvDepartamentos.getSelectionModel().getSelectedIndex() >= 0) {
-            dlgConfirmacao.setTitle("CONFIRMAÇÃO");
-            dlgConfirmacao.setContentText("Tem certeza que deseja excluir?\nEsta operação não poderá  ser desfeita.");
-            Optional<ButtonType> result = dlgConfirmacao.showAndWait();
-            if (result.isPresent() && result.get() == bttSim) {
+            Alertas.exibirAlerta(Alert.AlertType.CONFIRMATION, "Confirmação", "Confirmar a Exclusão", "Confirmar a exclusão?/n A Ação Não Poderá Ser Revertida");
+            if (Alert.AlertType.CONFIRMATION.equals(true)) {
                 departamento.delete(tbvDepartamentos.getSelectionModel().getSelectedItem());
-                btnProcurar.fire();
-                dlgMensagem.setTitle("Sucesso na Exclusão");
-                dlgMensagem.setContentText("Departamento Excluido com Sucesso!!");
-                dlgMensagem.showAndWait();
+                Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Sucesso", "Departamento Excluído", "Departamento Excluido com Sucesso");
+                inicializarForm();
             }
         } else {
-            dlgMensagem.setTitle("ERRO DE Seleção");
-            dlgMensagem.setContentText("É preciso selecionar um departamentoe da tabela para poder realizar a exclusÃ£o.");
-            dlgMensagem.showAndWait();
+            Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Erro", "Erro de Seleção", "Selecione um Departamento para Excluir");
         }
     }
-    
+
     @FXML
     void btnExcluir_onKeyPressed(KeyEvent event) {
-        
+        if (event.getCode() == KeyCode.ENTER) {
+            btnExcluir.fire();
+        }
     }
-    
+
     @FXML
     void btnNovoChefeDep_onAction(ActionEvent event) {
-        
+
     }
-    
+
     @FXML
     void btnNovoChefeDep_onKeyPressed(KeyEvent event) {
-        
+
     }
-    
+
     @FXML
     void btnNovoDep_onAction(ActionEvent event) {
         habilitarEdicao(true);
@@ -210,46 +209,53 @@ public class FrmCadastroDepartamento implements Initializable {
         limparFormulario();
         novo = true;
     }
-    
+
     @FXML
     void btnNovoDep_onKeyPressed(KeyEvent event) {
-        
+        if (event.getCode() == KeyCode.ENTER) {
+            btnNovoDep.fire();
+        }
     }
-    
+
     @FXML
     void btnProcurar_onAction(ActionEvent event) {
-      if(txtPesqNomeDep.getText().trim().equals("") && !txtPesqSiglaDep.getText().trim().equals("")){
-          tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).getBySigla(txtPesqSiglaDep.getText())));
-      }else if(!txtPesqNomeDep.getText().trim().equals("") && txtPesqSiglaDep.getText().trim().equals("")){
-        tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).getByNome(txtPesqNomeDep.getText())));
-      }else Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Busca", "Pesquisa não pode ser vazio", "Digite a Sigla ou o Nome do Departamento para Prosseguir com a Busca");
-     // tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).getBySigla("sigla")));
+        if (txtPesqNomeDep.getText().trim().equals("") && !txtPesqSiglaDep.getText().trim().equals("")) {
+            tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).getBySigla(txtPesqSiglaDep.getText())));
+        } else if (!txtPesqNomeDep.getText().trim().equals("") && txtPesqSiglaDep.getText().trim().equals("")) {
+            tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).getByNome(txtPesqNomeDep.getText())));
+        } else {
+            Alertas.exibirAlerta(Alert.AlertType.INFORMATION, "Busca", "Pesquisa não pode ser vazio", "Digite a Sigla ou o Nome do Departamento para Prosseguir com a Busca");
+        }
+        // tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).getBySigla("sigla")));
 
-		
-
-		tbpDepartamento.getSelectionModel().select(tabConsultarDepartamento);
+        tbpDepartamento.getSelectionModel().select(tabConsultarDepartamento);
     }
-    
+
     @FXML
     void btnSair2_onAction(ActionEvent event) {
         limparFormulario();
+        inicializarForm();
     }
-    
+
     @FXML
     void btnSair2_onKeyPressed(KeyEvent event) {
-        
+        if (event.getCode() == KeyCode.ENTER) {
+            btnSair2.fire();
+        }
     }
-    
+
     @FXML
     void btnSair_onAction(ActionEvent event) {
-        
+        System.exit(0);
     }
-    
+
     @FXML
     void btnSair_onKeyPressed(KeyEvent event) {
-        
+        if (event.getCode() == KeyCode.ENTER) {
+            btnSair.fire();
+        }
     }
-    
+
     @FXML
     void tbvDepartamentos_onMouseClicked(MouseEvent event) {
         DepartamentoEntity ent = (DepartamentoEntity) tbvDepartamentos.getSelectionModel().getSelectedItem();
@@ -260,7 +266,7 @@ public class FrmCadastroDepartamento implements Initializable {
             txtObs.setText(ent.getObservacao());
             btnAlterar.setDisable(false);
             btnExcluir.setDisable(false);
-            
+
         }
         if (event.getClickCount() > 1) {
             tbpDepartamento.getSelectionModel().select(tabGerenciarDepartamento);
@@ -270,35 +276,24 @@ public class FrmCadastroDepartamento implements Initializable {
             txtObs.setDisable(true);
             btnNovoChefeDep.setDisable(true);
             btnConfirmar.setDisable(true);
-            
-            
+
         }
     }
-    
+
     void habilitarEdicao(boolean sim) {
         btnNovoDep.setDisable(sim);
         btnConfirmar.setDisable((!sim));
         btnAlterar.setDisable(sim);
         btnSair.setDisable((!sim));
         btnExcluir.setDisable(sim);
-        
+
         txtNovoNomeDep.setDisable(!sim);
         txtNovaSiglaDep.setDisable(!sim);
+        txtObs.setDisable(!sim);
         chbChefeDep.setDisable(!sim);
     }
-    
-    private void limparFormulario() {
-        txtNovoNomeDep.setText("");
-        txtNovaSiglaDep.setText("");
-        //txtTelefone.setText("");
-    }
-    
-    @Override
-    public void initialize(java.net.URL location, ResourceBundle resources) {
-        tbcChefe.setCellValueFactory(new PropertyValueFactory<>("chefe"));
-        tbcNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        tbcSigla.setCellValueFactory(new PropertyValueFactory<>("sigla"));
-        chbChefeDep.setItems(FXCollections.observableArrayList( MockServidorDAO.getInstance().buscarTodosServidores()));
+
+    void inicializarForm() {
         tbvDepartamentos.setItems(FXCollections.observableArrayList(new Departamento(DAOFactory.getDAOFactory().getDepartamentoDAO()).buscarTodos()));
         btnNovoDep.setDisable(false);
         btnExcluir.setDisable(true);
@@ -306,6 +301,23 @@ public class FrmCadastroDepartamento implements Initializable {
         btnProcurar.setDisable(false);
         btnSair.setDisable(false);
         btnConfirmar.setDisable(false);
+
+    }
+
+    private void limparFormulario() {
+        txtNovoNomeDep.setText("");
+        txtNovaSiglaDep.setText("");
+        txtObs.setText("");
+        //txtTelefone.setText("");
+    }
+
+    @Override
+    public void initialize(java.net.URL location, ResourceBundle resources) {
+        tbcChefe.setCellValueFactory(new PropertyValueFactory<>("chefe"));
+        tbcNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tbcSigla.setCellValueFactory(new PropertyValueFactory<>("sigla"));
+        chbChefeDep.setItems(FXCollections.observableArrayList(MockServidorDAO.getInstance().buscarTodosServidores()));
+        inicializarForm();
     }
 
 }
